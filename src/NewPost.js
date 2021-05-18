1b8b1128548f71004023c325bb5b0bfff53d1f6e
zuzut152 <talzuzut3@gmail.com>
2021-05-18 18:30:36 +0300

    EX6_UPDATE

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,14 +1,62 @@
 import React from 'react';
+import axios from "axios";
 
 export default class NewPost extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: '',
+            publishedBy: '',
+            publishedDays: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+        console.log("changed ", nam, val);
+    }
+
+    handleSubmit(event) {
+        console.log("state-", this.state)
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/posts',
+            data: {
+                ...this.state
+            }
+        });
+        window.alert("Successfully added Post by- " + this.state.publishedBy);
+    }
+
     render() {
         return (
             <tr>
                 <td>
                     <h1> Here you would insert <b>NEW POST </b></h1>
+                    <form>
+                        <label>
+                            Post Content:
+                            <input type="text" name="content" onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Author:
+                            <input type="text" name="publishedBy" onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            How many days ago?:
+                            <input type="text" name="publishedDays" onChange={this.handleChange}/>
+                        </label>
+                        <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+                    </form>
                 </td>
             </tr>
+
         );
+
     }
 
 }
--- src/PostPage.js
+++ src/PostPage.js
@@ -2,21 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
-class MainMenu extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td>
-                    <span><a href={""}> Home </a>|</span>
-                    <span> <a href={""}>About Me </a> |</span>
-                    <span> <a href={""}>Contact Me </a></span>
-
-                </td>
-                <td className='login'><a href= './Login.js'> Login </a></td>
-            </tr>);
-    }
-}
 
 class MainHeader extends React.Component {
     render() {
@@ -33,31 +20,17 @@ class MainHeader extends React.Component {
     }
 }
 
-class PopularHeader extends React.Component {
-    render() {
-        return (
-            <h1 className='rightSide' id='popular'> Popular</h1>
-        );
-    }
-}
-
 
 class PostComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            number: "",
-            content: "",
-            publishedDays: "",
-            publishedBy: "",
-        };
     }
 
     render() {
         return (
             <td className='whole_border'>
       <span>
-        <h3>Blog post {this.props.number} </h3>
+        <h3>Blog post {this.props.id} </h3>
           {this.props.content}
       </span>
                 <br/> <br/> <br/>
@@ -68,80 +41,42 @@ class PostComponent extends React.Component {
     }
 }
 
-class LatestPosts extends React.Component {
+export default class Container extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: "",
-            link: "",
+            posts_list: []
         };
     }
 
-    render() {
-        return (
-            <span> Blog Post #{this.props.number} <a href={this.props.link}> go to page</a> <br/> <br/>
-      </span>);
+    componentDidMount() {
+        axios.get(' http://localhost:5000/posts').then(response => {
+
+            if (response && response.data && response.status === 200) {
+                this.setState({
+                    posts_list: response.data
+                })
+
+            }
+        });
     }
-}
 
-const post1 = {
-    number: "1",
-    content: `My  first blog post 
-        is all about my  blog post
-    and how to write a new post in my  blog, you can find it  here. `,
-    publishedDays: "1",
-    publishedBy: "Israel"
-}
-
-const post2 = {
-    number: "2",
-    content: ` My  second blog post 
-                 is all about my blog post`,
-    publishedDays: "2",
-    publishedBy: "Joe"
-}
-const blog1 = {
-    number: '1',
-    link: ""
-}
-const blog2 = {
-    number: '2',
-    link: ""
-}
-const blog3 = {
-    number: '3',
-    link: ""
-}
-
-
-export default class Container extends React.Component {
     render() {
+        const postsList =
+            this.state.posts_list.map(post =>
+
+                <tr><PostComponent {...post}/></tr>
+            )
+        console.log(postsList)
         return (
             <table className='border_space' id='MainTable'>
-
-                {/*<MainMenu/>*/}
+                <thead>
                 <MainHeader/>
-                <tr>
-                    <PostComponent {...post1} />
-                    <td className='border_bottom'>
-                        <LatestPosts {...blog1} />
-                        <LatestPosts {...blog2} />
-                        <LatestPosts {...blog3} />
-                    </td>
-                </tr>
-                <tr>
-                    <PostComponent {...post2} />
-                    <td className='border_bottom'>
-                        <PopularHeader/>
-                        <LatestPosts {...blog3} />
-                        <LatestPosts {...blog1} />
-                        <LatestPosts {...blog2} />
-                    </td>
-                </tr>
-
+                </thead>
+                <tbody>
+                {postsList}
+                </tbody>
             </table>
-
-
         );
     }
 }
